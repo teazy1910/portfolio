042910750a27dfeb6baea538d021a015cc0b8022
tanja dietz <tanjaisabel.dietz@gmail.com>
2023-10-05 09:29:38 +0200

    update icons and skills

---
+++ node_modules/lucide/dist/esm/icons/copyleft.js
@@ -0,0 +1,17 @@
+/**
+ * lucide v0.284.0 - ISC
+ */
+
+import defaultAttributes from '../defaultAttributes.js';
+
+const Copyleft = [
+  "svg",
+  defaultAttributes,
+  [
+    ["circle", { cx: "12", cy: "12", r: "10" }],
+    ["path", { d: "M9 9.4a4 4 0 1 1 0 5.25" }]
+  ]
+];
+
+export { Copyleft as default };
+//# sourceMappingURL=copyleft.js.map
---
+++ node_modules/lucide/dist/esm/icons/copyright.js
@@ -0,0 +1,17 @@
+/**
+ * lucide v0.284.0 - ISC
+ */
+
+import defaultAttributes from '../defaultAttributes.js';
+
+const Copyright = [
+  "svg",
+  defaultAttributes,
+  [
+    ["circle", { cx: "12", cy: "12", r: "10" }],
+    ["path", { d: "M15 9.4a4 4 0 1 0 0 5.246" }]
+  ]
+];
+
+export { Copyright as default };
+//# sourceMappingURL=copyright.js.map
